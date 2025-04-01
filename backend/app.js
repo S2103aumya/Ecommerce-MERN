74ca9d095df0462a08e0371164659d3ffb0130c9
SAUMYA JAIN <132819769+S2103aumya@users.noreply.github.com>
2025-04-01 15:24:13 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -113,7 +113,6 @@ const cors = require("cors"); // Import CORS at the top
 
 const allowedOrigins = [
     "https://ecommercemern-f.onrender.com"
-    "http://localhost:5173",  // For local development (if using Vite)
     "http://localhost:2103"   // If using Create React App
 ];
 
